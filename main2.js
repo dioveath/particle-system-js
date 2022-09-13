52fbf86854257988e2ef1725e8e57b3d63cdfffa
gurungsandesh <sangrg226@gmail.com>
2022-09-13 21:42:35 +0545

    Features added

--- main2.js
+++ main2.js
@@ -1,15 +1,26 @@
-window.onload = function(){
+window.onload = function () {
 
   var canvas = document.getElementById("canvas"),
-  context = canvas.getContext("2d"),
-  width = canvas.width = window.innerWidth,
-  height = canvas.height = window.innerHeight;
+    context = canvas.getContext("2d"),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;
+
+  var selectColorContainer = document.getElementsByClassName("color");
+  let colorExternal = "#00FFAB";
+
+  for (let index = 0; index < selectColorContainer.length; index++) {
+    selectColorContainer[index].addEventListener('click', () => {
+      colorExternal = selectColorContainer[index].style.backgroundColor;
+    })
+
+  }
+
 
   var startTime = new Date();
 
   ParticlePool.init();
 
-  var pEffect = particleEffect.create(width/2, height/2, 2000, 4, true, true, 2, 6, 0, 100, 0, -Math.PI);
+  var pEffect = particleEffect.create(width / 2, height / 2, 2000, 4, true, true, 2, 6, 0, 100, 0, -Math.PI);
   pEffect.randomize();
   // pEffect.numParticles = 1000;
   // pEffect.resetEffect();
@@ -26,7 +37,7 @@ window.onload = function(){
     prevx: 0, prevy: 0,
     scale: 50,
 
-    getSpeed: function(){
+    getSpeed: function () {
       var dx = this.prevx - this.x;
       var dy = this.prevy - this.y;
       return Math.sqrt(dx * dx + dy * dy);
@@ -45,17 +56,17 @@ window.onload = function(){
     timeRange: 1,
     stormRange: 0.05,
 
-    update: function(deltaTime){
-      this.tLerpValue += (1/this.tTime) * deltaTime; //1 is the distance of lerp
-      if(this.tLerpValue <= 1){
+    update: function (deltaTime) {
+      this.tLerpValue += (1 / this.tTime) * deltaTime; //1 is the distance of lerp
+      if (this.tLerpValue <= 1) {
         this.x = utils.lerp(this.tLerpValue, this.x, this.targetx);
         this.y = utils.lerp(this.tLerpValue, this.y, this.targety);
       } else {
-        if(Math.random() * 1 < this.stormRange){
+        if (Math.random() * 1 < this.stormRange) {
           this.targetx = utils.randomRange(-this.rangeValue * 3, this.rangeValue * 3);
           this.targety = utils.randomRange(-this.rangeValue * 3, this.rangeValue * 3);
           this.tLerpValue = 0;
-          this.tTime = utils.randomRange(0, this.timeRange/3);
+          this.tTime = utils.randomRange(0, this.timeRange / 3);
         } else {
           this.targetx = utils.randomRange(-this.rangeValue, this.rangeValue);
           this.targety = utils.randomRange(-this.rangeValue, this.rangeValue);
@@ -67,22 +78,22 @@ window.onload = function(){
 
   };
 
-  var airBlowAngle = Math.PI/2;
+  var airBlowAngle = Math.PI / 2;
   var airBlowThrust = 2000;
 
   var numTimes = 1;
 
   update();
-  function update(){
+  function update() {
 
-    var deltaTime = (new Date() - startTime) /1000;
+    var deltaTime = (new Date() - startTime) / 1000;
     startTime = new Date();
-    if(deltaTime > 0.1) deltaTime = 0.1;
+    if (deltaTime > 0.1) deltaTime = 0.1;
 
     Wind.update(deltaTime);
 
-    for(var j = 0; j < numTimes; j++){
-      for(var i = allParticleEffects.length - 1; i >= 0; i --){
+    for (var j = 0; j < numTimes; j++) {
+      for (var i = allParticleEffects.length - 1; i >= 0; i--) {
         allParticleEffects[i].applyVortex(vortex);
         allParticleEffects[i].applyForce(Wind.x, Wind.y);
         allParticleEffects[i].update(deltaTime);
@@ -95,18 +106,17 @@ window.onload = function(){
 
     context.fillStyle = "rgba(0, 0, 0, 1)";
     context.fillRect(0, 0, width, height);
-    for(var j = allParticleEffects.length - 1; j >= 0; j --){
+    for (var j = allParticleEffects.length - 1; j >= 0; j--) {
       var effect = allParticleEffects[j];
-      for(var i = 0; i < effect.allParticles.length; i++){
+      for (var i = 0; i < effect.allParticles.length; i++) {
         var p = effect.allParticles[i];
-        var alpha = p.internalTime/effect.duration;
-        var color = "rgba(255, 0, " + Math.floor((1-alpha) * 255) + ", " + (1 - Math.round(alpha*100)/100) + ")";
+        var alpha = p.internalTime / effect.duration;
+        var color = colorExternal;
         context.fillStyle = color;
         context.beginPath();
         context.arc(p.x, p.y, p.radius, 0, Math.PI * 2, false);
         context.fill();
       }
-
     }
 
 
@@ -117,15 +127,15 @@ window.onload = function(){
 
 
 
-  document.addEventListener("click", function(e){
-    if(numTimes == 50){
-      numTimes = 1;
-    } else {
-      numTimes = 50;
-    }
-  },false);
+  // document.addEventListener("click", function (e) {
+  //   if (numTimes == 50) {
+  //     numTimes = 1;
+  //   } else {
+  //     numTimes = 50;
+  //   }
+  // }, false);
 
-  document.addEventListener("mousemove", function(e){
+  document.addEventListener("mousemove", function (e) {
     vortex.prevx = vortex.x;
     vortex.prevy = vortex.y;
     vortex.x = e.clientX;
--- particle.js
+++ particle.js
@@ -12,7 +12,7 @@ var particle = {
   gravity: 0,
   internalTime: 0,
 
-  create: function(x, y, speed, direction, gravity, friction){ //gravity is/should be G * mass of planet/radius of planet (squared)
+  create: function (x, y, speed, direction, gravity, friction) { //gravity is/should be G * mass of planet/radius of planet (squared)
     var obj = Object.create(this);
     obj.x = x;
     obj.y = y;
@@ -26,32 +26,32 @@ var particle = {
     return obj;
   },
 
-  getSpeed: function(){
+  getSpeed: function () {
     return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
   },
 
-  setSpeed: function(speed){
+  setSpeed: function (speed) {
     var heading = this.getHeading();
     this.vx = Math.cos(heading) * speed;
     this.vy = Math.sin(heading) * speed;
   },
 
-  getHeading: function(){
+  getHeading: function () {
     return Math.atan2(this.vy, this.vx);
   },
 
-  setHeading: function(heading){
+  setHeading: function (heading) {
     var speed = this.getSpeed();
     this.vx = Math.cos(heading) * speed;
     this.vy = Math.sin(heading) * speed;
   },
 
-  setSpeedDirection: function(speed, direction){
+  setSpeedDirection: function (speed, direction) {
     this.vx = Math.cos(direction) * speed;
     this.vy = Math.sin(direction) * speed;
   },
 
-  applyForce: function(ax, ay){
+  applyForce: function (ax, ay) {
     //as force is accumalated we will add these forces and
     //we will reset it in last update method
     ax /= this.mass;
@@ -60,7 +60,7 @@ var particle = {
     this.ay += ay;
   },
 
-  update: function(deltaTime){
+  update: function (deltaTime) {
     // this.handleSprings();
     // this.handleGravity();
     this.ay += this.gravity * this.mass;
